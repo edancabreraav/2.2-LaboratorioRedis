e49f624546405925729fe72c1e49818ee3d3df9a
edancabreraav <edancabreraav@ittepic.edu.mx>
2024-10-11 17:02:59 -0700

    clientes listo ahora sí

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -20,3 +20,25 @@ exports.addClient = async (req, res) => {
         return res.status(500).json({ message: 'Error al agregar el cliente', error });
     }
 };
+
+// Función para listar el conjunto de clientes de una sucursal
+exports.getClients = async (req, res) => {
+    const { sucursalId } = req.params;
+
+    // Armar la clave con el formato: SUCURSAL:1000:CLIENTES
+    const redisKey = `SUCURSAL:${sucursalId}:CLIENTES`;
+
+    try {
+        // Obtener el conjunto de clientes
+        const clientSet = await conexion.sMembers(redisKey);
+
+        if (!clientSet || clientSet.length === 0) {
+            return res.status(404).json({ message: 'Conjunto vacío' });
+        }
+
+        return res.status(200).json(clientSet);
+    } catch (error) {
+        console.error('Error al obtener el conjunto:', error);
+        return res.status(500).json({ message: 'Error al obtener el conjunto', error });
+    }
+};
--- routes/client.js
+++ routes/client.js
@@ -4,5 +4,8 @@ const clientController = require('../controllers/clientController');
 
 // Ruta para agregar un cliente y asociarlo a una sucursal
 router.post('/add', clientController.addClient);
+// Ruta para obtener el conjunto de clientes de una sucursal
+router.get('/sucursal/:sucursalId/clientes', clientController.getClients);
+
 
 module.exports = router;
